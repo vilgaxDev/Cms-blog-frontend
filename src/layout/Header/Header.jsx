23666e6b01526e668a007bfa72d60589c449851d
vilgax <ken.vilgax@gmail.com>
2024-02-02 19:18:48 +0300

    header theme switch

--- src/layout/Header/Header.jsx
+++ src/layout/Header/Header.jsx
@@ -2,7 +2,7 @@ import { NavLink, Link } from 'react-router-dom'
 import React, { useState } from 'react';
 import Logo from '../../components/Logo/Logo';
 import Button from '../../components/Buttons/Button';
-import ThemeSwitch from '../../components/ThemeSwitch/themeSwitch';
+// import ThemeSwitch from '../../components/ThemeSwitch/themeSwitch';
 import NavBar from '../../components/NavBar/Navbar';
 import './Header.css'
 
@@ -22,13 +22,13 @@ const Header = () => {
                             link="/"
                             className="btn primary_btn"
                         />
-                        <ThemeSwitch />
-                        <Button
+                        {/* <ThemeSwitch /> */}
+                        {/* <Button
                             text={
                                 <svg viewBox="0 0 24 24" width="28" height="28" stroke="currentColor" strokeWidth="2" fill="none" strokeLinecap="round" strokeLinejoin="round"><line x1="3" y1="12" x2="21" y2="12"></line><line x1="3" y1="6" x2="21" y2="6"></line><line x1="3" y1="18" x2="21" y2="18"></line></svg>
                             }
                             className={`toggle`}
-                        />
+                        /> */}
                     </div>
                 </div>
             </header >
